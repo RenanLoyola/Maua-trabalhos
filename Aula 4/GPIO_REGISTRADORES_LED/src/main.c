134ed7931ebf7c50b06c56410dec91bc48bd2504
aaaa <aaa@gmail.com>
2016-03-18 18:08:02 -0300

    PISCA OS 3 LED SIMULTANEOS

--- Aula 4/GPIO_REGISTRADORES_LED/src/main.c
+++ Aula 4/GPIO_REGISTRADORES_LED/src/main.c
@@ -32,6 +32,10 @@
  * ----------------------------------
  */
 #define PIN_LED_BLUE 19
+#define PIN_LED_GREEN 20
+#define PIN_LED_RED 20
+
+
 
 /**
  * Main function
@@ -59,28 +63,33 @@ int main (void)
 		
 	// 29.17.4 PMC Peripheral Clock Enable Register 0
 	// 1: Enables the corresponding peripheral clock.
-	// ID_PIOA = 11 - TAB 11-1
-	PMC->PMC_PCER0 |= ID_PIOA;
+	// ID_PIOA = 11 - TAB 11-1 
+	PMC->PMC_PCER0 |= ID_PIOA | ID_PIOC;
 
 	 //31.6.1 PIO Enable Register
 	// 1: Enables the PIO to control the corresponding pin (disables peripheral control of the pin).	
-	PIOA->PIO_PER |= (1 << PIN_LED_BLUE );
+	PIOA->PIO_PER |= (1 << PIN_LED_BLUE )|(1 << PIN_LED_GREEN );
+	PIOC->PIO_PER |= (1 << PIN_LED_RED );
 
 	// 31.6.46 PIO Write Protection Mode Register
 	// 0: Disables the write protection if WPKEY corresponds to 0x50494F (PIO in ASCII).
 	PIOA->PIO_WPMR = 0;
+	PIOC->PIO_WPMR = 0;
 	
 	// 31.6.4 PIO Output Enable Register
 	// value =
 	//	 	1 : Enables the output on the I/O line.
 	//	 	0 : do nothing
-	PIOA->PIO_OER |=  (1 << PIN_LED_BLUE );
+	PIOA->PIO_OER |=  (1 << PIN_LED_BLUE )| (1 << PIN_LED_GREEN ) ;
+	PIOC->PIO_OER |=  (1 << PIN_LED_RED );
 
 	// 31.6.10 PIO Set Output Data Register
 	// value = 
 	// 		1 : Sets the data to be driven on the I/O line.
 	// 		0 : do nothing
-	//PIOA->PIO_SODR = (1 << PIN_LED_BLUE );
+	// PIOA->PIO_CODR = (1 << PIN_LED_GREEN );
+	// PIOC->PIO_SODR = (1 << PIN_LED_RED );
+	
 	// implementacao do PIO_CODR
 	 
 	/**
@@ -92,9 +101,12 @@ int main (void)
              * Utilize a função delay_ms para fazer o led piscar na frequência
              * escolhida por você.
              */
-			PIOA->PIO_SODR = (1 << PIN_LED_BLUE );
-            delay_ms(500);
-			PIOA ->PIO_CODR = (1 << PIN_LED_BLUE) ;
+			//LED AZUL+VERDE PISCA
+			PIOA->PIO_SODR = (1 << PIN_LED_BLUE )|(1 << PIN_LED_GREEN );
+			PIOC->PIO_CODR = (1 << PIN_LED_RED );
+			delay_ms(500);
+			PIOA ->PIO_CODR = (1 << PIN_LED_BLUE) |(1 << PIN_LED_GREEN ) ;
+			PIOC ->PIO_SODR = (1 << PIN_LED_RED) ;
 			delay_ms(500);
 		
 	}
